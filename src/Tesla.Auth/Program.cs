df0dd530d6cd0807b4d67de67879727a5f689ced
at0dd <atodd@atodd.io>
2022-06-25 12:41:25 -0500

    Add base auth project

---
+++ src/Tesla.Auth/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");;
+
+app.Run();
+
