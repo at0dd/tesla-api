c363c30ed96f08982002a2a7d66955218e0d8444
Joe Cool <15796035+mavnorthwind@users.noreply.github.com>
2022-08-06 15:23:42 +0200

    Update SpeedLimitMode.cs
    Trying to query the vehicle state throws a JsonException:
    Input string '50.0' is not a valid integer. Path 'response.speed_limit_mode.min_limit_mph', line 1, position 915.
    SpeedLimitMode.MinimumLimitMilesPerHour and SpeedLimitMode.MaximumLimitMilesPerHour seem to be doubles, not ints.
    Just like CurrentLimitMilesPerHour.
    
    hth
    mav

--- src/Tesla.API/Models/SpeedLimitMode.cs
+++ src/Tesla.API/Models/SpeedLimitMode.cs
@@ -14,10 +14,10 @@
         public double CurrentLimitMilesPerHour { get; set; }
 
         [JsonProperty("max_limit_mph")]
-        public int MaximumLimitMilesPerHour { get; set; }
+        public double MaximumLimitMilesPerHour { get; set; }
 
         [JsonProperty("min_limit_mph")]
-        public int MinimumLimitMilesPerHour { get; set; }
+        public double MinimumLimitMilesPerHour { get; set; }
 
         [JsonProperty("pin_code_set")]
         public bool PINCodeSet { get; set; }
