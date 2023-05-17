554a64177029a4c92174a24de1b27e5f1773b0e9
at0dd <atodd@atodd.io>
2023-05-17 00:15:38 -0500

    Cleanup energy models

--- src/Models/Engery/EnergySite.cs
+++ src/Models/Engery/EnergySite.cs
@@ -31,25 +31,4 @@ namespace TeslaAPI.Models.Engery
         [JsonProperty("components")]
         public EnergySiteComponents Components { get; set; }
     }
-
-    public class EnergySiteComponents
-    {
-        [JsonProperty("battery")]
-        public bool Battery { get; set; }
-
-        [JsonProperty("solar")]
-        public bool Solar { get; set; }
-
-        [JsonProperty("solar_type")]
-        public string SolarType { get; set; }
-
-        [JsonProperty("grid")]
-        public bool Grid { get; set; }
-
-        [JsonProperty("load_meter")]
-        public bool LoadMeter { get; set; }
-
-        [JsonProperty("market_type")]
-        public string MarketType { get; set; }
-    }
 }
--- src/Models/Engery/EnergySitePowerHistory.cs
+++ src/Models/Engery/EnergySitePowerHistory.cs
@@ -1,6 +1,5 @@
 namespace TeslaAPI.Models.Engery
 {
-    using System;
     using System.Collections.Generic;
     using Newtonsoft.Json;
 
@@ -16,29 +15,5 @@ namespace TeslaAPI.Models.Engery
         /// </summary>
         [JsonProperty("time_series")]
         public List<EnergySitePowerHistoryTime> TimeSeries { get; set; } = new List<EnergySitePowerHistoryTime>();
-    }
-
-    public class EnergySitePowerHistoryTime
-    {
-        [JsonProperty("timestamp")]
-        public DateTime Timestamp { get; set; }
-
-        /// <summary>
-        /// Gets or sets the power being generated, in watts, at the given timestamp.
-        /// </summary>
-        [JsonProperty("solar_power")]
-        public int SolarPower { get; set; }
-
-        [JsonProperty("battery_power")]
-        public int BatteryPower { get; set; }
-
-        [JsonProperty("grid_power")]
-        public int GridPower { get; set; }
-
-        [JsonProperty("grid_services_power")]
-        public int GridServicesPower { get; set; }
-
-        [JsonProperty("generator_power")]
-        public int GeneratorPower { get; set; }
     }
 }
